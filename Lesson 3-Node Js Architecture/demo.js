a2e1517050e0c1b97976171cba7b9bd2c0a38d7f
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-25 19:36:05 +0600

    Node JS NPM with express

---
+++ Lesson 3-Node Js Architecture/demo.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const start = Date.now();
+    while (Date.now() - start < 2000) {}
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
+setTimeout(() => {
+    console.log('This is a non-blocking task executed after 1 second');
+}, 1000);
+
+console.log('Server started. Waiting for requests...');
