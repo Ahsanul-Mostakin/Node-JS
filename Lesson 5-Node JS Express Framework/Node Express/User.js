5fc9372ffe149ef5e0919c8636317471b9041f6f
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-27 02:02:34 +0600

    Node JS Express Framework(Create,Delete & Update records)

---
+++ Lesson 5-Node JS Express Framework/Node Express/User.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+      id: 1,
+      name: 'mostakin',
+      email: 'mostakin@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: 3,
+      name: 'Jack',
+      email: 'jack@gmail.com',
+    },
+    {
+      id: 4,
+      name: 'Chris',
+      email: 'chris@gmail.com',
+    },
+  ];
+  
+  module.exports = users;
